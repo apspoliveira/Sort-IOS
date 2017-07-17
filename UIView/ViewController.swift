aba45ae11892f1bb100ae6bb080a899a0f9dd875
antoniopedro <apspoliveira@gmail.com>
2017-07-17 12:28:48 +0100

    Initial Commit

---
+++ UIView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIView
+//
+//  Created by António Pedro on 14/07/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, world!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
