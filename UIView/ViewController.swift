0a6c2f68072cd74e8eaa8e0f9abdd5e78ed6e64c
antoniopedro <apspoliveira@gmail.com>
2017-07-17 15:05:58 +0100

    ...

--- UIView/ViewController.swift
+++ UIView/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello, world!")
+        let finalSquare = 25
+        var board = [Int](repeating: 0, count: finalSquare + 1)
+        
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
