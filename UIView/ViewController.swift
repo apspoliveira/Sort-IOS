c2e70fe488b6d0dfc7ecd35b4bb65b12a9453ae2
antoniopedro <apspoliveira@gmail.com>
2017-07-18 15:55:07 +0100

    ...

--- UIView/ViewController.swift
+++ UIView/ViewController.swift
@@ -7,38 +7,51 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        var friends:[Friend] = []
         
-        let finalSquare = 25
-        var board = [Int](repeating: 0, count: finalSquare + 1)
+        let friend1 = Friend(name: "Sergey", age: 30)
+        let friend2 = Friend(name: "Bill", age: 35)
+        let friend3 = Friend(name: "Michael", age: 21)
         
-        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
-        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+        friends.append(friend1)
+        friends.append(friend2)
+        friends.append(friend3)
         
-        var square = 0
-        var diceRoll = 0
-        while square < finalSquare {
-            // roll the dice
-            diceRoll += 1
-            if diceRoll == 7 { diceRoll = 1 }
-            // move by the rolled amount
-            square += diceRoll
-            if square < board.count {
-                // if we're still on the board, move up or down for a snake or a ladder
-                square += board[square]
-            }
-        }
-        print("Game over!");
+        // Get sorted array in descending order (largest to the smallest number)
+        let sortedFriends = friends.sorted(by: { $0.age > $1.age })
+        printFriends(friends: sortedFriends)
         
+        // Get sorted array in ascending order (smallest to the largest number)
+        let sortedFriendsAscendingOrder = friends.sorted(by: { $0.age < $1.age })
+        printFriends(friends: sortedFriendsAscendingOrder)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func printFriends(friends: [Friend])
+    {
+        for friendEntry in friends {
+            print("Name: \(friendEntry.name), age: \(friendEntry.age)")
+        }
+    }
 }
 
+class Friend {
+    let name : String
+    let age : Int
+    
+    init(name : String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
